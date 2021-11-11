e3afbff9d46aedbe744bda266caad64ed4d97a11
Rauchdimehdi <rauchdi18@gmail.com>
2021-11-11 14:29:54 +0100

    Added the car link to the data scrapped

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 1. After providing a link, you get a list of information of the cars in this format :
 python3 web-scrapping.py
 ```
-the car has 22 850 km, modele 03/2017 with 74 kW (101 CH) -- PRICE -- € 13 795,-  
-the car has 74 852 km, modele 09/2017 with 55 kW (75 CH) -- PRICE -- € 13 490,-  
-the car has 34 750 km, modele 09/2018 with 55 kW (75 CH) -- PRICE -- € 13 800,-  
+the car offer :
+ /offres/peugeot-208-82-style-puretech-panoramadach-pdc-essence-bleu-9b5bbbb7-70c4-46b9-ad7b-1751d68ec683 has 39 100 km, 
+modele 01/2017 with 60 kW (82 CH) -- PRICE -- € 9 880,-  
+the car offer :
+ /offres/peugeot-208-active-klima-zv-einparkhilfe-tempomat-essence-noir-933aabe1-1dfc-4442-9610-75243f039264 has 86 000 km, 
+modele 03/2017 with 60 kW (82 CH) -- PRICE -- € 7 999,-  
+the car offer :
+ /offres/peugeot-208-active-1-2-puretech-82-klima-sitzheizung-tempomat-essence-blanc-455f83ec-b795-49b9-9475-c1c06b2b8d01 has 51 200 km, 
+modele 03/2017 with 60 kW (82 CH) -- PRICE -- € 8 900,-  
 ```
--- web-scrapping.py
+++ web-scrapping.py
@@ -10,9 +10,13 @@ import requests
 import urllib3
 import re
 
-url = "https://www.autoscout24.fr/lst/toyota/yaris/60311-innenstadt-(frankfurt-am-main)?sort=standard&desc=0&offer=D%2CJ%2CO%2CU&fuel=2&ustate=N%2CU&size=20&page=1&lon=8.6790758&lat=50.1129&zip=60311%20Innenstadt%20(Frankfurt%20am%20Main)&zipr=50&cy=D&priceto=15000&fregfrom=2016&atype=C&fc=6&qry=&"
+yaris = "https://www.autoscout24.fr/lst/toyota/yaris/60311-innenstadt-(frankfurt-am-main)?sort=standard&desc=0&offer=D%2CJ%2CO%2CU&fuel=2&ustate=N%2CU&size=20&page=1&lon=8.6790758&lat=50.1129&zip=60311%20Innenstadt%20(Frankfurt%20am%20Main)&zipr=50&cy=D&priceto=15000&fregfrom=2016&atype=C&fc=6&qry=&"
+peugeot = "https://www.autoscout24.fr/lst/peugeot/208/60311-innenstadt-(frankfurt-am-main)?sort=standard&desc=0&offer=D%2CJ%2CO%2CU&ustate=N%2CU&size=20&page=1&lon=8.6790758&lat=50.1129&zip=60311%20Innenstadt%20(Frankfurt%20am%20Main)&zipr=50&cy=D&priceto=12500&kmto=90000&fregfrom=2017&atype=C&fc=3&qry=&"
+clio = "https://www.autoscout24.fr/lst/renault/clio/60311-innenstadt-(frankfurt-am-main)?sort=standard&desc=0&offer=D%2CJ%2CO%2CU&fuel=D&ustate=N%2CU&size=20&page=1&lon=8.6790758&lat=50.1129&zip=60311%20Innenstadt%20(Frankfurt%20am%20Main)&zipr=150&cy=D&priceto=12500&kmto=100000&fregfrom=2015&atype=C&fc=31&qry=&"
+ds3 = ""
+polo = "https://www.autoscout24.fr/lst/volkswagen/polo-(tous)/60311-innenstadt-(frankfurt-am-main)?sort=standard&desc=0&offer=D%2CJ%2CO%2CU&fuel=D&ustate=N%2CU&size=20&page=1&lon=8.6790758&lat=50.1129&zip=60311%20Innenstadt%20(Frankfurt%20am%20Main)&zipr=150&cy=D&priceto=12500&kmto=100000&fregfrom=2015&atype=C&fc=27&qry=&"
 
-html_text = requests.get(url).text
+html_text = requests.get(peugeot).text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all("div", class_="cl-list-element cl-list-element-gap")
 for job in jobs:
@@ -20,8 +24,13 @@ for job in jobs:
     car_details = job.find('div', class_='cldt-summary-vehicle-data').text
     info = job.ul.find_all('li')
     info_list = [str(li) for li in info]
+    link_extraction = job.find('div', class_='cldt-summary-titles')
+    link = link_extraction.a['href']
+
     km = info_list[0].splitlines()[1]
     year = info_list[1].splitlines()[1]
     hp = info_list[2].splitlines()[1]
-    print(f'the car has {km}, modele {year} with {hp} -- PRICE -- {price.strip()}  ')
+
+    print(f'the car offer :\n{link} has {km}, \nmodele {year} with {hp} -- PRICE -- {price.strip()}  ')
+
 
